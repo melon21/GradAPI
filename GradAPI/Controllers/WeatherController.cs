52fec1d3881a859b934da2c2c7f7a6664aaa815b
Melinda Marino <melindamarino22@gmail.com>
2020-03-12 18:46:04 -0500

    GradAPI

---
+++ GradAPI/Controllers/WeatherController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GradAPI.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
