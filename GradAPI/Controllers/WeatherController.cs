122c86380a59ce0b659dd687461636d9a2328b86
Melinda Marino <melindamarino22@gmail.com>
2020-03-12 20:21:43 -0500

    API grad ver1

--- GradAPI/Controllers/WeatherController.cs
+++ GradAPI/Controllers/WeatherController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,12 @@ namespace GradAPI.Controllers
 {
     public class WeatherController : Controller
     {
+        static HttpClient Client = new HttpClient();
+
         public IActionResult Index()
         {
-            return View();
+            var response = Client.GetAsync("https://dataservice.accuweather.com/forecasts/v1/daily/1day/331131?apikey=RF9jPHpFC8IXHNAZUGrdC9BPQO93mzI8&language=en-us&details=false&metric=false").Result;
+            return new JsonResult(response.Content.ReadAsAsync<object>().Result);
         }
     }
 }
